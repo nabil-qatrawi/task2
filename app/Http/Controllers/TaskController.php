b193de240a96e34d9b4d86f810560823502ee8df
nabil-qatrawi <nn0162381@gmail.com>
2021-11-13 20:05:40 +0200

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class TaskController extends Controller
+{
+
+    public function index() {
+    $tasks =DB::table('tasks')->get();  
+      return view ('tasks' , compact('tasks'));
+    }
+
+
+    public function store(Request $request) {
+        DB::table('tasks')->insert([
+       'name'=>$request->name,
+        'created_at'=> now(),   
+        'updated_at'=> now()
+        ]);
+   
+   return 'store';
+                           
+   }
+   
+}
+
+
